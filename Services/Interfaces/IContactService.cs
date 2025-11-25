dd5799f3f27443e8317c4679b3f7a0ca2c0c2cda
pujannagari-pallavi <164523092+pujannagari-pallavi@users.noreply.github.com>
2025-11-25 15:26:19 +0530

    Delete Services directory

--- Services/Interfaces/IContactService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Wedding_Planner.Application.Services.Interfaces
-{
-    public interface IContactService
-    {
-        Task SubmitContactFormAsync(string name, string email, string subject, string message);
-    }
-}
--- Services/Interfaces/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace Wedding_Planner.Application.Services.Interfaces
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string toEmail, string subject, string body);
-    }
-}
